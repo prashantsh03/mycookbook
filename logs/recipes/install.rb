42942df1bca7dd13ab055930b546baf63da1c749
prashantsh03 <er.prashantsh03@gmail.com>
2016-11-08 18:30:24 +0530

    Update install.rb
    added region and updated download URL

--- logs/recipes/install.rb
+++ logs/recipes/install.rb
@@ -1,13 +1,19 @@
+template "/tmp/cwlogs.cfg" do
+  source "cwlogs.cfg.erb"
+  owner "root"
+  group "root"
+  mode 0644
+end
 directory "/opt/aws/cloudwatch" do
  recursive true
 end
 
 remote_file "/opt/aws/cloudwatch/awslogs-agent-setup-v1.0.py" do
-source "https://s3.amazonaws.com/aws-cloudwatch/downloads/awslogs-agent-setup-v1.0.py"
+source "https://s3.amazonaws.com/aws-cloudwatch/downloads/latest/awslogs-agent-setup.py"
 mode "0755"
 end
 
 execute "Install CloudWatch Logs agent" do
-command "/opt/aws/cloudwatch/awslogs-agent-setup-v1.0.py -n -r us-east-1 -c /tmp/cwlogs.cfg"
+command "/opt/aws/cloudwatch/awslogs-agent-setup.py -n -r ap-southeast-1 -c /tmp/cwlogs.cfg"
 not_if { system "pgrep -f aws-logs-agent-setup" }
 end
