77d3a0703c99b6e0c6a4d787aa657f57b9168753
prashantsh03 <er.prashantsh03@gmail.com>
2016-11-08 19:44:55 +0530

    Update install.rb
    URL changed

--- STG-WEB/recipes/install.rb
+++ STG-WEB/recipes/install.rb
@@ -2,12 +2,12 @@ directory "/opt/aws/cloudwatch" do
  recursive true
 end
 
-remote_file "/opt/aws/cloudwatch/awslogs-agent-setup-v1.0.py" do
-source "https://s3.amazonaws.com/aws-cloudwatch/downloads/awslogs-agent-setup-v1.0.py"
+remote_file "/opt/aws/cloudwatch/awslogs-agent-setup.py" do
+source "https://s3.amazonaws.com/aws-cloudwatch/downloads/latest/awslogs-agent-setup.py"
 mode "0755"
 end
 
 execute "Install CloudWatch Logs agent" do
-command "/opt/aws/cloudwatch/awslogs-agent-setup-v1.0.py -n -r us-east-1 -c /tmp/cwlogs.cfg"
+command "/opt/aws/cloudwatch/awslogs-agent-setup.py -n -r ap-southeast-1 -c /tmp/cwlogs.cfg"
 not_if { system "pgrep -f aws-logs-agent-setup" }
 end
