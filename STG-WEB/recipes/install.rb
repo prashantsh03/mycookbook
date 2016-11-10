f0f7a5d68cbdd680c6fe0b5d18c34e7eff8feeea
prashantsh03 <er.prashantsh03@gmail.com>
2016-11-10 14:51:40 +0530

    Update install.rb

--- STG-WEB/recipes/install.rb
+++ STG-WEB/recipes/install.rb
@@ -1,10 +1,3 @@
-template "/tmp/cwlogs.cfg" do
-  cookbook "logs"
-  source "cwlogs.cfg.erb"
-  owner "root"
-  group "root"
-  mode 0644
-end
 directory "/opt/aws/cloudwatch" do
  recursive true
 end
